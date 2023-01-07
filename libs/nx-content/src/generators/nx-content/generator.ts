08bcd401bebcf94b8627b12c7dd44dd556ddb4c4
Aaron Miller <drkstr101@gmail.com>
2023-01-07 22:41:08 +0000

    feat: content library generator

--- libs/nx-content/src/generators/nx-content/generator.ts
+++ libs/nx-content/src/generators/nx-content/generator.ts
@@ -1,73 +1,285 @@
 import {
+  addDependenciesToPackageJson,
   addProjectConfiguration,
+  convertNxGenerator,
+  ensurePackage,
+  extractLayoutDirectory,
   formatFiles,
   generateFiles,
+  GeneratorCallback,
   getWorkspaceLayout,
+  joinPathFragments,
   names,
   offsetFromRoot,
+  ProjectConfiguration,
+  toJS,
   Tree,
+  updateJson,
 } from '@nrwl/devkit';
-import * as path from 'path';
+import { Linter } from '@nrwl/linter';
+import { getImportPath } from 'nx/src/utils/path';
+import { join } from 'path';
+import { runTasksInSerial } from '../../utilities/run-tasks-in-serial';
+import {
+  getRelativePathToRootTsConfig,
+  getRootTsConfigPathInTree,
+} from '../../utilities/typescript';
 import { NxContentGeneratorSchema } from './schema';
 
-interface NormalizedSchema extends NxContentGeneratorSchema {
-  projectName: string;
+const nxVersion = '15.4.5';
+
+export interface NormalizedSchema extends NxContentGeneratorSchema {
+  name: string;
+  fileName: string;
   projectRoot: string;
   projectDirectory: string;
+  libsDir: string;
   parsedTags: string[];
+  importPath?: string;
 }
+
+function addProject(tree: Tree, options: NormalizedSchema) {
+  const projectConfiguration: ProjectConfiguration = {
+    root: options.projectRoot,
+    sourceRoot: joinPathFragments(options.projectRoot, 'src'),
+    projectType: 'library',
+    targets: {},
+    tags: options.parsedTags,
+  };
+
+  if (options.buildable) {
+    addDependenciesToPackageJson(tree, {}, { '@nrwl/js': nxVersion });
+    projectConfiguration.targets.build = {
+      executor: '@nrwl/js:tsc',
+      outputs: ['{options.outputPath}'],
+      options: {
+        outputPath:
+          options.libsDir != '.'
+            ? `dist/${options.libsDir}/${options.projectDirectory}`
+            : `dist/${options.projectDirectory}`,
+        main: `${options.projectRoot}/src/index` + (options.js ? '.js' : '.ts'),
+        tsConfig: `${options.projectRoot}/tsconfig.lib.json`,
+        assets: [`${options.projectRoot}/*.md`],
+      },
+    };
+  }
+
+  addProjectConfiguration(
+    tree,
+    options.name,
+    projectConfiguration,
+    options.standaloneConfig
+  );
+}
+
+export async function addLint(
+  tree: Tree,
+  options: NormalizedSchema
+): Promise<GeneratorCallback> {
+  await ensurePackage(tree, '@nrwl/linter', nxVersion);
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { lintProjectGenerator } = require('@nrwl/linter');
+  return lintProjectGenerator(tree, {
+    project: options.name,
+    linter: options.linter,
+    skipFormat: true,
+    tsConfigPaths: [
+      joinPathFragments(options.projectRoot, 'tsconfig.lib.json'),
+    ],
+    unitTestRunner: options.unitTestRunner,
+    eslintFilePatterns: [
+      `${options.projectRoot}/**/*.${options.js ? 'js' : 'ts'}`,
+    ],
+    setParserOptionsProject: options.setParserOptionsProject,
+  });
+}
+
+function updateTsConfig(tree: Tree, options: NormalizedSchema) {
+  updateJson(tree, join(options.projectRoot, 'tsconfig.json'), (json) => {
+    if (options.strict) {
+      json.compilerOptions = {
+        ...json.compilerOptions,
+        forceConsistentCasingInFileNames: true,
+        strict: true,
+        noImplicitReturns: true,
+        noFallthroughCasesInSwitch: true,
+      };
+    }
+
+    return json;
+  });
+}
+
+function updateRootTsConfig(host: Tree, options: NormalizedSchema) {
+  updateJson(host, getRootTsConfigPathInTree(host), (json) => {
+    const c = json.compilerOptions;
+    c.paths = c.paths || {};
+    delete c.paths[options.name];
+
+    if (c.paths[options.importPath]) {
+      throw new Error(
+        `You already have a library using the import path "${options.importPath}". Make sure to specify a unique one.`
+      );
+    }
+
+    c.paths[options.importPath] = [
+      joinPathFragments(
+        options.projectRoot,
+        './src',
+        'index.' + (options.js ? 'js' : 'ts')
+      ),
+    ];
+
+    return json;
+  });
+}
+
+function createFiles(tree: Tree, options: NormalizedSchema) {
+  const { className, name, propertyName } = names(options.name);
+
+  const rootOffset = offsetFromRoot(options.projectRoot);
+  generateFiles(tree, join(__dirname, './files/lib'), options.projectRoot, {
+    ...options,
+    dot: '.',
+    className,
+    name,
+    propertyName,
+    js: !!options.js,
+    cliCommand: 'nx',
+    strict: undefined,
+    tmpl: '',
+    offsetFromRoot: rootOffset,
+    rootTsConfigPath: getRelativePathToRootTsConfig(tree, options.projectRoot),
+    hasUnitTestRunner: options.unitTestRunner !== 'none',
+    hasLinter: options.linter !== 'none',
+  });
+
+  if (options.unitTestRunner === 'none') {
+    tree.delete(
+      join(options.projectRoot, 'src/lib', `${options.fileName}.spec.ts`)
+    );
+  }
+
+  if (options.skipBabelrc) {
+    tree.delete(join(options.projectRoot, '.babelrc'));
+  }
+
+  if (options.js) {
+    toJS(tree);
+  }
+
+  if (!options.buildable) {
+    tree.delete(join(options.projectRoot, 'package.json'));
+  }
+
+  updateTsConfig(tree, options);
+}
+
+async function addJest(
+  tree: Tree,
+  options: NormalizedSchema
+): Promise<GeneratorCallback> {
+  await ensurePackage(tree, '@nrwl/jest', nxVersion);
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { jestProjectGenerator } = require('@nrwl/jest');
+  return await jestProjectGenerator(tree, {
+    ...options,
+    project: options.name,
+    setupFile: 'none',
+    supportTsx: true,
+    babelJest: options.babelJest,
+    skipSerializers: true,
+    testEnvironment: options.testEnvironment,
+    skipFormat: true,
+  });
+}
+
+export async function libraryGenerator(
+  tree: Tree,
+  schema: NxContentGeneratorSchema
+) {
+  const options = normalizeOptions(tree, schema);
+
+  createFiles(tree, options);
+
+  if (!options.skipTsConfig) {
+    updateRootTsConfig(tree, options);
+  }
+  addProject(tree, options);
+
+  const tasks: GeneratorCallback[] = [];
+
+  if (options.linter !== 'none') {
+    const lintCallback = await addLint(tree, options);
+    tasks.push(lintCallback);
+  }
+  if (options.unitTestRunner === 'jest') {
+    const jestCallback = await addJest(tree, options);
+    tasks.push(jestCallback);
+  }
+
+  if (!options.skipFormat) {
+    await formatFiles(tree);
+  }
+
+  return runTasksInSerial(...tasks);
+}
+
+export default libraryGenerator;
+export const librarySchematic = convertNxGenerator(libraryGenerator);
 
 function normalizeOptions(
   tree: Tree,
   options: NxContentGeneratorSchema
 ): NormalizedSchema {
   const name = names(options.name).fileName;
-  const projectDirectory = options.directory
-    ? `${names(options.directory).fileName}/${name}`
-    : name;
+  const { layoutDirectory, projectDirectory } = extractLayoutDirectory(
+    options.directory ? `${names(options.directory).fileName}/${name}` : name
+  );
+
+  if (!options.unitTestRunner) {
+    options.unitTestRunner = 'jest';
+  }
+
+  if (!options.linter) {
+    options.linter = Linter.EsLint;
+  }
+
   const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
-  const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
+  const fileName = getCaseAwareFileName({
+    fileName: options.simpleModuleName ? name : projectName,
+    pascalCaseFiles: options.pascalCaseFiles,
+  });
+
+  const { libsDir: defaultLibsDir, npmScope } = getWorkspaceLayout(tree);
+  const libsDir = layoutDirectory ?? defaultLibsDir;
+
+  const projectRoot = joinPathFragments(libsDir, projectDirectory);
+
   const parsedTags = options.tags
     ? options.tags.split(',').map((s) => s.trim())
     : [];
 
+  const importPath =
+    options.importPath || getImportPath(npmScope, projectDirectory);
+
   return {
     ...options,
-    projectName,
+    fileName,
+    name: projectName,
     projectRoot,
     projectDirectory,
     parsedTags,
+    importPath,
+    libsDir,
   };
 }
 
-function addFiles(tree: Tree, options: NormalizedSchema) {
-  const templateOptions = {
-    ...options,
-    ...names(options.name),
-    offsetFromRoot: offsetFromRoot(options.projectRoot),
-    template: '',
-  };
-  generateFiles(
-    tree,
-    path.join(__dirname, 'files'),
-    options.projectRoot,
-    templateOptions
-  );
-}
+function getCaseAwareFileName(options: {
+  pascalCaseFiles: boolean;
+  fileName: string;
+}) {
+  const normalized = names(options.fileName);
 
-export default async function (tree: Tree, options: NxContentGeneratorSchema) {
-  const normalizedOptions = normalizeOptions(tree, options);
-  addProjectConfiguration(tree, normalizedOptions.projectName, {
-    root: normalizedOptions.projectRoot,
-    projectType: 'library',
-    sourceRoot: `${normalizedOptions.projectRoot}/src`,
-    targets: {
-      build: {
-        executor: '@watheia/waweb.nx-content:build',
-      },
-    },
-    tags: normalizedOptions.parsedTags,
-  });
-  addFiles(tree, normalizedOptions);
-  await formatFiles(tree);
+  return options.pascalCaseFiles ? normalized.className : normalized.fileName;
 }
